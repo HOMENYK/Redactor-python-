5b319b3df793af4c8317927046889e85dbbde1f2
HOMENYK <homenko25032011@gmail.com>
2024-07-29 19:17:54 +0700

    Fixed AutoUpdater

--- source code/Main.py
+++ source code/Main.py
@@ -23,17 +23,38 @@ Sharpness - 1
 -------------------------------------------------------------------------------------------------------------------
 ''')
 import os
-os.system("AutoUpdate.exe")
 import numpy as np
 from tkinter import Menu, messagebox
 from tkinter.ttk import Combobox
 from PIL import Image,ImageTk,ImageEnhance,ImageFilter
 import os
 from tqdm import tqdm
+import githubdl
+#Updater V3, credit to: (discord) sania212"
+print("Updater V3, credit to: (discord) sania212")
 window = Tk()
 window["bg"] = "black"
 window.title("PythonShop")
-window.geometry('900x650')
+window.geometry('1000x700')
+try:
+    githubdl.dl_file("https://github.com/HOMENYK/Redactor-python-", "Main.exe", "Main_temp.exe", github_token="...")
+except:
+    print("Bad internet connection")
+print("Checking for updates, please wait...")
+Mfile = open("Main.exe", "rb")
+Current_code = Mfile.read()
+Mfile.close()
+Mfile2 = open("Main_temp.exe", "rb")
+Git_code = Mfile2.read()
+Mfile2.close()
+if(Git_code == Current_code):
+    print("Your version already up to date")
+else:
+    if(str(input("Update found, are you want to install update? (T or F): ")) == "T"):
+        os.system("AutoUpdate.exe")
+        window.destroy
+    else:
+        pass
 def Saved():
     lbl2=Label(window, text='Saved!', font=('Arial Bold', 8, ), bg='grey',fg='white')
     lbl2.place(x=70,y=25)
